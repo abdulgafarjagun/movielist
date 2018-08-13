a00499930493d97b1a60a7c25e004f1d6458de9e
kobiyashimaru2030 <abdulgafar.jagun@andela.com>
2018-08-13 18:23:49 +0100

    bad commit

--- scrapper/getWebPage.js
+++ scrapper/getWebPage.js
@@ -1,24 +1,25 @@
 var http = require('http');
 
-var options = {
-    host: 'www.google.com',
-    path: '/',
-    method: 'get',
-    headers:{
-        'Content-Type' : 'text/html'
+    var options = {
+        host: 'silverbirdcinemas.com',
+        path: '/cinema/accra/',
+        method: 'get',
+        headers:{
+            'Content-Type' : 'text/html'
+        }
     }
-}
+        var html = '';
 
-var getUrlAsWebPage = (opt) => {
-    var html = '';
-    var req = http.request(opt, (res) => {
-        res.on('data', (chunk) => {
-            html += chunk;
-        })
-        res.on('end', () => {
-            console.log(html);
-        });
-    }).end();
-}
+        http.request(options, (res) => {
+            res.on('data', (chunk) => {
+                html += chunk;
+            })
+            res.on('end', () => {
+                console.log(html);
+            });
+        }).end();
+        
 
-module.export = getUrlAsWebPage(options);
+
+
+
--- scrapper/index.js
+++ scrapper/index.js
@@ -1,2 +1,11 @@
+var JSSoup = require('jssoup').default;
+var getUrlAsWebPage = require('./getWebPage');
+
+var html = getUrlAsWebPage;
+
+console.log(html);
+
+// var soup = new JSSoup(getUrlAsWebPage);
+
 
 
