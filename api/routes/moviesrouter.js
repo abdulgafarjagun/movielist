9c5df721d2ec089547d785b183abcc553b508157
kobiyashimaru2030 <abdulgafar.jagun@andela.com>
2018-08-14 02:33:53 +0100

    added comment to moviesrouter

--- api/routes/moviesrouter.js
+++ api/routes/moviesrouter.js
@@ -1,8 +1,15 @@
 var movieRouter = require('express').Router();
 
+
 movieRouter.route('/movies')
+    // 'GET /movie/' : {
+    // 'desc' : 'get all movies',
+    // 'response' : '200 application/json',
+    // 'data' : '{}, {}, {}'
+    //}
     .get((req, res) => {
         res.send("hello world from home");
-    });
+    })
+    
 
 module.exports = movieRouter;
